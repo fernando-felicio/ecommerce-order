6dc7335db84d09f0d343075ed76b5ee567eb64d4
Fernando Felicio <fernandofelicio.dev@gmail.com>
2024-01-24 17:01:51 -0300

    Creating OrderItem class

---
+++ src/application/Program.java
@@ -0,0 +1,22 @@
+package application;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Program {
+	
+	public static void main (String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		
+		
+		
+		scanner.close();
+		
+		
+	}
+
+}
---
+++ src/entities/OrderItem.java
@@ -0,0 +1,39 @@
+package entities;
+
+public class OrderItem {
+	
+	private Integer quantity;
+	private Double price;
+	
+	public OrderItem() {
+		
+	}
+	
+	public OrderItem(Integer quantity, Double price) {
+		this.quantity = quantity;
+		this.price = price;
+	}
+	
+	public Integer getQuantity() {
+		return this.quantity;
+	}
+	
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	public Double subTotal(Integer quantity, Double price) {
+		Double sum = 0.0;
+		sum = this.quantity * this.price;
+		return sum;
+	}
+
+}
